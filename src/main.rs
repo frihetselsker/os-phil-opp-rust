1739073ee82295e9595bb7f99c0cb6036b5362dd
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-08-18 10:17:02 +0300

    Third post: VGA

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,19 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    println!("Hello, Ferris{}", "!");
     loop {}
 }
